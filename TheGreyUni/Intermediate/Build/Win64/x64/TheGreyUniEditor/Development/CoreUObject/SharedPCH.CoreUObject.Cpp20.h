427dfd081e32993d3bda9b75b650feb47ba9ed61
BMWebb03 <bmw129@student.aru.ac.uk>
2026-02-21 14:34:12 +0000

    commit from home

--- TheGreyUni/Intermediate/Build/Win64/x64/TheGreyUniEditor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h
-#include "C:/Users/bmw129/AppData/Local/Development/TheGreyUni/TheGreyUni/Intermediate/Build/Win64/x64/TheGreyUniEditor/Development/CoreUObject/SharedDefinitions.CoreUObject.Cpp20.h"
-#include "Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h"
--- TheGreyUni/Intermediate/Build/Win64/x64/TheGreyUniEditor/Development/UnrealEd/SharedPCH.UnrealEd.Project.ValApi.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
-#include "C:/Users/bmw129/AppData/Local/Development/TheGreyUni/TheGreyUni/Intermediate/Build/Win64/x64/TheGreyUniEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Project.ValApi.Cpp20.h"
-#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
